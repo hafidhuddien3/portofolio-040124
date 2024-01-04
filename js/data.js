d9e056697e97b97d6e1b2bc103aedd84ac5f579f
hafidhuddien3 <shubuhshubuh@gmail.com>
2024-01-04 10:36:03 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,86 @@
+const getInitialData = [
+
+{
+name: "Resto Mania",
+address: "https://main--spiffy-selkie-23d9e5.netlify.app/",
+image: "assets/images/image(1).png",
+feature: "Single-Page App PWA service worker caching,icon Add to Home Screen,webpack,Mobile First Approach Responsive,Aksesibilitas,JavaScript Clean Code,Automation Testing jest github action,end to end test codeceptjs,Image Optimization [sharp. ImageminWebpackPlugin. lazy loading],bundle analyzer,Code Splitting splitChunks"
+},
+
+{
+name: "Story App",
+address: "https://adorable-begonia-2cc860.netlify.app/",
+image: "assets/images/image(2).png",
+feature: "webpack,spinner dan placeholder,Authentication,HTTP Client dengan Axios,sass scss,bootstrap,Lit Element"
+},
+
+{
+name: "Food Finder",
+address: "https://spontaneous-strudel-4b4238.netlify.app",
+image: "assets/images/image(3).png",
+feature: "webpack,fetch,AJAX,custom element,Responsive"
+},
+
+{
+name: "Bookshelf App",
+address: "https://keen-tapioca-379b0f.netlify.app/",
+image: "assets/images/image(4).png",
+feature: "Responsive"
+},
+
+{
+name: "Kab. Tulungagung Info",
+address: "https://helpful-bombolone-50cd53.netlify.app/",
+image: "assets/images/image(5).png",
+feature: ""
+},
+
+{
+name: "Notes App React",
+address: "https://creative-kitsune-3e24ff.netlify.app/",
+image: "assets/images/image(6).png",
+feature: ""
+},
+
+{
+name: "forum api",
+address: "https://github.com/hafidhuddien3/forum-api",
+image: null,
+feature: "PostgreSQL,Clean Architecture,Jest,instances-container,postman"
+},
+
+{
+name: "open music api",
+address: "https://github.com/hafidhuddien3/open-music-api-be6-030124",
+image: null,
+feature: "PostgreSQL,redis CacheService,bcrypt,dotenv,joi,nanoid"
+},
+
+{
+name: "open music api consumer",
+address: "https://github.com/hafidhuddien3/open-music-api-consumer-030124",
+image: null,
+feature: "Nodemailer send emails,amqp RABBITMQ_SERVER, client/consumer"
+},
+
+];
+
+
+/*
+const no={
+name: "Resto Mania fe6",
+address: "https://main--spiffy-selkie-23d9e5.netlify.app/",
+image: null,
+feature: "Single-Page App PWA service worker caching,icon Add to Home Screen,webpack,Mobile First Approach Responsive,Aksesibilitas,JavaScript Clean Code,Automation Testing jest github action,end to end test codeceptjs,Image Optimization [sharp. ImageminWebpackPlugin. lazy loading],bundle analyzer,Code Splitting splitChunks"
+};
+const myArray = no.feature.split(",");
+console.log(myArray);
+
+data template
+const no={
+name: "",
+address: "",
+image: null,
+feature: ""
+};
+*/
--- js/script.js
+++ js/script.js
@@ -1,237 +1,53 @@
-/**
- * [
- *    {
- *      id: <int>
- *      title: <string>
- *      author: <string>
- *      year: <number>
- *      isComplete: <boolean>
- *    }
- * ]
- */
-const books = [];
 
-const RENDER_EVENT = 'render-book';
-const SAVED_EVENT = 'saved-book';
-const STORAGE_KEY = 'BOOK_APPS';
 
-function isStorageExist() /* boolean */ {
-	if (typeof(Storage) === undefined) {
-		alert('Browser kamu tidak mendukung local storage');
-		return false;
-	}
-	return true;
+const wrapper = document.querySelector('.wrapper');
+
+const createItemTemplate = (data) => {
+return `
+<div class="row-item item shadow">
+
+  <div class="container selesai" id="selesai" >
+    <div class="list-item" id="completed-books">
+${createRowRightTemplate(data)}
+    </div>
+  </div>
+
+  <div class="container" id="belum-selesai">
+  <div class="list-item" id="books">
+${createRowLeftTemplate(data)}
+  </div>
+</div>
+
+  </div>`
 }
 
-function generateId() {
-	return +new Date();
+const createRowLeftTemplate = (data) => {
+  return `<a href="${data.address}" target=”_blank” ><img class="img_port" src="${data.image}" alt="${data.address}"><a>`
 }
 
-function generateBookObject(id, title, author, year, isComplete) {
-	return {
-		id,
-		title,
-		author,
-		year,
-		isComplete
-	}
+const createRowRightTemplate = (data) => {
+  return `
+  <h4 class="">${data.name}</h4>
+<ul id="${data.name}" ></ul>
+<a href="${data.address}" target=”_blank” >link</a>`
 }
 
-function findBook(bookId) {
-	for (const bookItem of books) {
-		if (bookItem.id === bookId) {
-			return bookItem;
-		}
-	}
-	return null;
-}
-
-function findBookIndex(bookId) {
-	for (const index in books) {
-		if (books[index].id === bookId) {
-			return index;
-		}
-	}
-	return -1;
-}
-
-function makeBook(bookObject) {
-
-	const {
-		id,
-		title,
-		author,
-		year,
-		isComplete
-	} = bookObject;
-
-	const textTitle = document.createElement('h2');
-	textTitle.innerText = title;
-
-	const textAuthorYear = document.createElement('p');
-	textAuthorYear.innerHTML = `Penulis: ${author}<br>Tahun: ${year}`;
-
-	const textContainer = document.createElement('div');
-	textContainer.classList.add('inner');
-	textContainer.append(textTitle, textAuthorYear);
-
-	const container = document.createElement('div');
-	container.classList.add('item', 'shadow')
-	container.append(textContainer);
-	container.setAttribute('id', `book-${id}`);
-
-	if (isComplete) {
-
-		const undoButton = document.createElement('button');
-		undoButton.classList.add('undo-button');
-		undoButton.addEventListener('click', function() {
-			undoBookFromCompleted(id);
-		});
-
-		const trashButton = document.createElement('button');
-		trashButton.classList.add('trash-button');
-		trashButton.addEventListener('click', function() {
-			removeBookFromCompleted(id);
-		});
-
-		container.append(undoButton, trashButton);
-	} else {
-
-		const checkButton = document.createElement('button');
-		checkButton.classList.add('check-button');
-		checkButton.addEventListener('click', function() {
-			addBookToCompleted(id);
-		});
-
-		const trashButton = document.createElement('button');
-		trashButton.classList.add('trash-button');
-		trashButton.addEventListener('click', function() {
-			removeBookFromCompleted(id);
-		});
-
-		container.append(checkButton, trashButton);
-	}
-	return container;
-}
-
-function addBook() {
-	const title = document.getElementById('title').value;
-	const author = document.getElementById('author').value;
-	const year = parseInt(document.getElementById('year').value);
-	const isComplete = document.getElementById('isComplete').checked;
-
-	const generatedID = generateId();
-	const bookObject = generateBookObject(generatedID, title, author, year, isComplete)
-	books.push(bookObject);
-	document.dispatchEvent(new Event(RENDER_EVENT));
-	saveData();
-
-}
-
-function addBookToCompleted(bookId /* HTMLELement */ ) {
-
-	const bookTarget = findBook(bookId);
-	if (bookTarget == null) return;
-
-	bookTarget.isComplete = true;
-	document.dispatchEvent(new Event(RENDER_EVENT));
-	saveData();
-}
-
-function removeBookFromCompleted(bookId /* HTMLELement */ ) {
-	const bookTarget = findBookIndex(bookId);
-
-	if (bookTarget === -1) return;
-
-	books.splice(bookTarget, 1);
-	document.dispatchEvent(new Event(RENDER_EVENT));
-	saveData();
-}
-
-function undoBookFromCompleted(bookId /* HTMLELement */ ) {
-	const bookTarget = findBook(bookId);
-
-	if (bookTarget == null) return;
-
-	bookTarget.isComplete = false;
-	document.dispatchEvent(new Event(RENDER_EVENT));
-	saveData();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-	const submitForm /* HTMLFormElement */ = document.getElementById('form');
-
-	submitForm.addEventListener('submit', function(event) {
-		event.preventDefault();
-		addBook();
-	});
-
-	if (isStorageExist()) {
-		loadDataFromStorage();
-	}
-
-	const header = document.getElementsByTagName('header');
-
-	const trashButton = document.createElement('button');
-	trashButton.classList.add('trash-button');
-	trashButton.addEventListener('click', function() {
-
-		let type = prompt("melakukan localStorage.clear();?");
-		if (type != null) {
-			localStorage.removeItem(STORAGE_KEY);
-			localStorage.clear();
-			books.length = 0;
-			document.dispatchEvent(new Event(RENDER_EVENT));
-		}
-
-	});
-
-	header[0].append(trashButton);
-
-});
+const createListTemplate = (data) => {
+const uList = document.getElementById(data.name);
+const myArray = data.feature.split(",");
+myArray.forEach((item, index) => {
+      uList.innerHTML += `<li>`+item+`</li>`;
+    });}
 
 
-document.addEventListener(RENDER_EVENT, function() {
-	const unreadedBOOKList = document.getElementById('books');
-	const listCompleted = document.getElementById('completed-books');
 
-	// clearing list item
-	unreadedBOOKList.innerHTML = '';
-	listCompleted.innerHTML = '';
 
-	for (const bookItem of books) {
-		const bookElement = makeBook(bookItem);
-		if (bookItem.isComplete) {
-			listCompleted.append(bookElement);
-		} else {
-			unreadedBOOKList.append(bookElement);
-		}
-	}
-});
+    document.addEventListener('DOMContentLoaded', function() {
+      
+      getInitialData.forEach((item, index) => {
+        wrapper.innerHTML += createItemTemplate(item);
+  createListTemplate(item);
+      });
 
-function saveData() {
-	if (isStorageExist()) {
-		const parsed = JSON.stringify(books);
-		localStorage.setItem(STORAGE_KEY, parsed);
-		document.dispatchEvent(new Event(SAVED_EVENT));
+    });
 
-	}
-}
-
-document.addEventListener(SAVED_EVENT, function() {
-	//window.alert(localStorage.getItem(STORAGE_KEY));
-
-});
-
-function loadDataFromStorage() {
-	const serializedData = localStorage.getItem(STORAGE_KEY);
-	let data = JSON.parse(serializedData);
-
-	if (data !== null) {
-		for (const book of data) {
-			books.push(book);
-		}
-	}
-
-	document.dispatchEvent(new Event(RENDER_EVENT));
-}
